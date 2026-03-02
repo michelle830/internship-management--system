802cdadcaeb6f3978c8b74a01a21dea5eb6df2f9
sze lum <_szelum_@szes-MacBook-Air.local>
2026-03-02 15:05:58 +0800

    Update config.php - modified database settings

--- includes/config.php
+++ includes/config.php
@@ -16,6 +16,4 @@ $conn = new mysqli($host, $user, $password, $database);
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
-
-echo "Database connected successfully!";
 ?>
